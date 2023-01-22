97296cff18ff636871147c76076ad8804bc6af11
Radu Bagrin <bagrin.radu@gmail.com>
2023-01-22 15:01:45 +0000

    Added tailwind to FE

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './app-router';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <AppRouter />
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -6,7 +6,7 @@ import { PHASES_ROUTE } from '../routes';
 const Home = () => {
     const navigate = useNavigate();
     return <div>
-        <h1>HOME</h1>
+        <h1 className="text-3xl font-bold underline bg-red-500">HOME</h1>
         <div>
             <Button text="Go to Tracks" onClick={() => navigate(PHASES_ROUTE)} />
         </div>
